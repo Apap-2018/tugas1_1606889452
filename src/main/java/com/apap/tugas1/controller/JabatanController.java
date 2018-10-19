3663419deb6d2df74162cab61a365123124062e5
Rafie Dharmawan <rafiedharmawan@gmail.com>
2018-10-19 20:37:49 +0700

    bonus

--- src/main/java/com/apap/tugas1/controller/JabatanController.java
+++ src/main/java/com/apap/tugas1/controller/JabatanController.java
@@ -1,5 +1,7 @@
 package com.apap.tugas1.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.apap.tugas1.model.JabatanModel;
+import com.apap.tugas1.model.PegawaiModel;
 import com.apap.tugas1.service.JabatanService;
 
 @Controller
@@ -35,6 +38,10 @@ public class JabatanController {
 	private String view(@RequestParam(value="idJabatan") long id_jabatan, Model model) {
 		model.addAttribute("jabatan", jabatanService.getJabatanById(id_jabatan));
 		model.addAttribute("gaji", (long)jabatanService.getJabatanById(id_jabatan).getGajiPokok());
+		
+		List<PegawaiModel> pegawaiJabatan = jabatanService.getJabatanById(id_jabatan).getPegawaiList();
+        model.addAttribute("jumlahPegawai", pegawaiJabatan.size());
+        
 		return "view-jabatan";
 	}
 	
